b5561f06775013a6a8d1c4694afc0c75cd9f7ac0
cowboy8625 <cowboy8625@yahoo.com>
2019-12-27 23:52:43 -0600

    added camera

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,18 @@ use sdl2::mouse::{MouseButton, MouseState};
  
 const WIDTH: i32 = 900;
 const HEIGHT: i32 = 900;
-const GRID: i32 = 5;
+const GRID: i32 = 2;
+const MAX: i32 = 2;
+const MIN: i32 = 50;
+const STEP: i32 = 2;
 
 fn main() -> Result<(), String> {
+    let mut grid: i32 = 20; 
+    let mut width: i32 = WIDTH / grid;
+    let mut height: i32 = HEIGHT / grid;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
@@ -36,6 +45,7 @@ fn main() -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     'running: loop {
+        let screen = get_screen(x, y, width, height, &cells);
         let mouse = MouseState::new(&event_pump);
         for event in event_pump.poll_iter() {
             match event {
@@ -51,6 +61,24 @@ fn main() -> Result<(), String> {
                 Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
                     speed += 10;
                 }
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    if grid != MAX {
+                        if grid == MIN {
+                            x = 0;
+                            y = 0;
+                        }
+                        grid -= STEP;
+                        width = WIDTH / grid;
+                        height = HEIGHT / grid;
+                    }
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
+                    if grid != MIN {
+                        grid += STEP;
+                        width = WIDTH / grid;
+                        height = HEIGHT / grid;
+                    }
+                },
                 Event::KeyDown { keycode: Some(Keycode::R), .. } => {
                     cells = life_gen();
                 },
@@ -65,39 +93,52 @@ fn main() -> Result<(), String> {
                 },
                 Event::KeyDown { keycode: Some(Keycode::C), .. } => {
                     cells = clear();
-                    display_board(&mut canvas, &cells)?;
+                },
+                Event::KeyDown { keycode: Some(Keycode::K), .. } => {
+                    y = if y != 0 { y - 1 } else { y } 
+                },
+                Event::KeyDown { keycode: Some(Keycode::J), .. } => {
+                    y = if y + height < HEIGHT / GRID { y + 1 } else { y };
+                },
+                Event::KeyDown { keycode: Some(Keycode::L), .. } => {
+                    x = if x + width < WIDTH / GRID { x + 1 } else { x };
+                },
+                Event::KeyDown { keycode: Some(Keycode::H), .. } => {
+                    x = if x != 0 { x - 1 } else { x };
                 },
                 _ => {}
             }
         }
 
+        let screen = get_screen(x, y, width, height, &cells);
+        display_board(&mut canvas, &screen, grid)?;
         if mouse.left() {
             game_running = false;
-            let grid_x: usize = (mouse.x() / GRID) as usize;
-            let grid_y: usize = (mouse.y() / GRID) as usize;
-            cells[grid_y][grid_x] = block;
-            display_board(&mut canvas, &cells)?;
+            let screen_x: usize = (mouse.x() / grid) as usize;
+            let screen_y: usize = (mouse.y() / grid) as usize;
+            let (world_x, world_y) = screen_to_world_cords(x as usize, y as usize, screen_x, screen_y);
+            cells[world_y][world_x] = block;
+            display_board(&mut canvas, &screen, grid)?;
         }
 
         if game_running {
-            display_board(&mut canvas, &cells)?;
             cells = next_genoration(cells);
             ::std::thread::sleep(Duration::from_millis(speed));
-        }
+        } 
     }
     Ok(())
 }
 
-fn display_board(canvas: &mut Canvas<Window>, cells: &Vec<Vec<bool>>) -> Result<(), String> {
+fn display_board(canvas: &mut Canvas<Window>, cells: &Vec<Vec<bool>>, grid: i32) -> Result<(), String> {
     canvas.clear();
     for (y, row) in cells.iter().enumerate() {
         for (x, &alive) in row.iter().enumerate() {
             if alive {
                 canvas.set_draw_color(Color::RGB(0, 0, 0));
-                canvas.fill_rect(Rect::new(x as i32 * GRID, y as i32 * GRID, GRID as u32, GRID as u32))?; 
+                canvas.fill_rect(Rect::new(x as i32 * grid, y as i32 * grid, grid as u32, grid as u32))?; 
             } else {
                 canvas.set_draw_color(Color::RGB(255, 255, 255));
-                canvas.fill_rect(Rect::new(x as i32 * GRID, y as i32 * GRID, GRID as u32, GRID as u32))?; 
+                canvas.fill_rect(Rect::new(x as i32 * grid, y as i32 * grid, grid as u32, grid as u32))?; 
             }
         }
     }
@@ -105,6 +146,24 @@ fn display_board(canvas: &mut Canvas<Window>, cells: &Vec<Vec<bool>>) -> Result<
     Ok(())
 }
 
+fn get_screen(x: i32, y: i32, width: i32, height: i32, cells: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    let x = x as usize;
+    let y = y as usize;
+    let width = width as usize;
+    let height = height as usize;
+    (0..height)
+    .map(|dy|
+        (0..width)
+            .map(|dx| cells[y + dy][x + dx])
+            .collect()
+    )
+    .collect()
+}
+
+fn screen_to_world_cords(x: usize, y: usize, sx: usize, sy: usize) -> (usize, usize) {
+    let (x, y) = ((x + sx), (y + sy));
+    (x, y)
+}
 
 fn life_gen() -> Vec<Vec<bool>> {
     let mut v:Vec<Vec<bool>> = Vec::new();
